b4eb30be2422eff268af23e2302deea41d92d013
Cheney <ycyppq123@gmail.com>
2015-07-03 18:42:04 +0800

    对代码进行重构

---
+++ Weibo/Weibo/YCYTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  YCYTabBarViewController.h
+//  Weibo
+//
+//  Created by Cheney on 15/7/3.
+//  Copyright (c) 2015年 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCYTabBarViewController : UITabBarController
+
+@end
