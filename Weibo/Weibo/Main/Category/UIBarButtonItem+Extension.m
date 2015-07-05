d4ea92577d48e6eb4e12ca7a86c281d38d9454e8
Cheney <ycyppq123@gmail.com>
2015-07-05 21:46:11 +0800

    创建UIBarButtonItem的分类

---
+++ Weibo/Weibo/Main/Category/UIBarButtonItem+Extension.m
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+Extension.m
+//  Weibo
+//
+//  Created by Cheney on 15/7/5.
+//  Copyright (c) 2015年 Cheney. All rights reserved.
+//
+
+#import "UIBarButtonItem+Extension.h"
+
+@implementation UIBarButtonItem (Extension)
+
+@end
