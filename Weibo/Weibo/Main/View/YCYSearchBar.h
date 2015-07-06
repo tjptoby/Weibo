400f9e27fa1e00a6242f0fccba46706f90041326
Cheney <ycyppq123@gmail.com>
2015-07-06 17:01:12 +0800

    自定义搜索框

---
+++ Weibo/Weibo/Main/View/YCYSearchBar.h
@@ -0,0 +1,13 @@
+//
+//  YCYSearchBar.h
+//  Weibo
+//
+//  Created by Cheney on 15/7/6.
+//  Copyright (c) 2015年 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCYSearchBar : UITextField
+
+@end
