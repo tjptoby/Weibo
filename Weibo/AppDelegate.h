49632b3a01ddffccf080df9e407e720108ebf362
OrangeYoung <ycyppq123@gmail.com>
2015-10-15 15:11:19 +0800

    Init

---
+++ Weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weibo
+//
+//  Created by Cheney on 15/10/15.
+//  Copyright (c) 2015年 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo
+//
+//  Created by Cheney on 15/10/15.
+//  Copyright (c) 2015年 Cheney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
